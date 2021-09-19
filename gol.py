415c7fcef7b8925a73eea6d1eb8298b17f85b078
Roar Emaus <roar@emaus.no>
2021-09-19 17:07:26 +0200

    added helper functions for generating gliders and cross. changed functions for animation and creating pngs

--- gol.py
+++ gol.py
@@ -14,8 +14,31 @@ class Board:
         self.n = n
         self.m = m
         self.grid = np.zeros(shape=(n, m), dtype=int)
-        self.counter = itertools.count()
-        self.i_step = 0
+        self.counter = itertools.count(0)
+        self.i_step = next(self.counter)
+        self.fig, self.ax = None, None
+
+    def _create_glider(self, direction='down_right'):
+        glider = np.array([
+                    [0, 1, 0],
+                    [0, 0, 1],
+                    [1, 1, 1],
+                    ])
+        if direction == 'down_right':
+            return glider
+        elif direction == 'up_right':
+            return np.rot90(glider)
+        elif direction == 'down_left':
+            return np.rot90(np.rot90(np.rot90(glider)))
+        elif direction == 'up_left':
+            return np.rot90(np.rot90(glider))
+    
+    def _create_cross(self):
+        return np.array([
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+            ])
 
     def initialise_mid(self):
         mid_row = self.n//2
@@ -32,6 +55,22 @@ class Board:
     def initialise_every_other_col(self):
         for j in range(int(np.round(self.m/2))):
             self.grid[:, j*2] = 1
+
+    def initialise_colliding_gliders(self):
+        mid_row = self.n//2
+        mid_col = self.m//2
+        down_right = self._create_glider(direction='down_right')
+        down_left = self._create_glider(direction='down_left')
+        up_right = self._create_glider(direction='up_right')
+        up_left = self._create_glider(direction='up_left')
+        self.grid[0:3, 0:3] = down_right
+        self.grid[self.n-3:self.n, 0:3] = up_right
+        self.grid[self.n-3:self.n, self.m-3:self.m] = up_left
+        self.grid[0:3, self.m-3:self.m] = down_left
+
+        cross = self._create_cross()
+        self.grid[mid_row-1:mid_row+2, mid_col-1:mid_col+2] = cross
+
 
     def find_neighbours(self, i, j):
         above_row = i - 1
@@ -82,9 +121,17 @@ class Board:
         self.i_step = next(self.counter)
 
     def plot_grid(self):
-        self.ax.matshow(self.grid)
+        fig, ax = plt.subplots(figsize=(19.2, 10.8))
+        ax.matshow(self.grid)
+        print(self.i_step)
         plt.savefig(f"step_{self.i_step:04d}.png")
         plt.close(fig)
+
+    def generate_pngs(self, steps):
+        self.plot_grid()
+        for i in range(steps):
+            self.step()
+            self.plot_grid()
 
     def init_plot(self):
         self.fig, self.ax = plt.subplots(figsize=(19.2, 10.8))
@@ -96,21 +143,27 @@ class Board:
         self.ln.set_data(self.grid)
         return [self.ln]
 
+    def run_animation(self, repeat=False, interval=20):
+        from matplotlib import rcParams
 
+        # configure full path for ImageMagick
+        rcParams['animation.convert_path'] = r'/usr/bin/convert'
 
-board = Board(100, 100)
-board.initialise_mid()
-board.initialise_every_other_row()
-#board.initialise_every_other_col()
-board.init_plot()
-ani = FuncAnimation(board.fig, board.update_plot, frames=range(10),
-                    interval=20, blit=True)
-plt.show()
+        board.init_plot()
+        ani = FuncAnimation(
+                self.fig,
+                self.update_plot,
+                interval=interval,
+                blit=True,
+                repeat=repeat)
 
-
-
-
-
-
-
-
+        plt.show()
+    
+if __name__ == "__main__":
+    board = Board(20, 20)
+    #board.initialise_mid()
+    #board.initialise_every_other_row()
+    #board.initialise_every_other_col()
+    board.initialise_colliding_gliders()
+    #board.run_animation()
+    board.generate_pngs(steps=40)
